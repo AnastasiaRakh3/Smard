ecb6976b39ff0885b93c3e8a06b66543e9d6bcd0
Anastasiia Rakhmatova <rakhmatova-93@mail.ru>
2022-10-01 21:11:15 +0300

    Rename main scss file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-import './App.scss';
+import './index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
