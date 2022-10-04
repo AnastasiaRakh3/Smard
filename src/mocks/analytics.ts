835630b3006a64640db0f0c650666f0d5fbaa336
Anastasiia Rakhmatova <rakhmatova-93@mail.ru>
2022-10-04 18:18:42 +0300

    Add mock data

--- src/mocks/analytics.ts
+++ src/mocks/analytics.ts
@@ -9,5 +9,19 @@ export const analyticsDataJson = `[
   {
     "message": "Authorization required",
     "status_code": 401
+  },
+  {
+    "message": {
+      "Прибыль": "16.02%",
+      "Стандартное отклонение": "1.37%"
+    },
+    "status_code": 200
+  },
+  {
+    "message": {
+      "Прибыль": "201.11%",
+      "Стандартное отклонение": "3.17%"
+    },
+    "status_code": 200
   }
 ]`;
